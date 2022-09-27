81970e4b250f1b9a48c81ccc6265fe14a013d3a9
dr-NHA <56168811+dr-NHA@users.noreply.github.com>
2022-09-27 17:04:15 +0930

    Add files via upload

---
+++ NHA_CreateShortcut/NHA_CreateShortcut/Program.cs
@@ -0,0 +1,36 @@
+﻿using IWshRuntimeLibrary;
+using System;
+using System.IO;
+
+namespace NHA_CreateShortcut{
+    internal class Program{
+        static void Main(string[] args){
+            if(args.Length == 0){
+                Environment.Exit(0);
+            }else{
+                var CMD = args[0].Split(',');
+                string shortcutName = CMD[0];
+                string shortcutPath = CMD[1];
+                string Description = CMD[2];
+                string ApplicationToShortcut = CMD[3];
+CreateAppShortcut(shortcutName,shortcutPath, ApplicationToShortcut,Description);
+            }
+        }
+
+public static void CreateAppShortcut(string shortcutName, string shortcutPath, string ApplicationToShortcut,string Description){
+string shortcutLocation =Path.Combine(shortcutPath, shortcutName + ".lnk");
+
+if(System.IO.File.Exists(shortcutLocation))
+System.IO.File.Delete(shortcutLocation);
+
+WshShell shell = new WshShell();
+IWshShortcut shortcut = ((IWshShortcut)shell.CreateShortcut(shortcutLocation));
+shortcut.Description = Description;// The description of the shortcut
+shortcut.IconLocation = ApplicationToShortcut;// The icon of the shortcut
+shortcut.TargetPath = ApplicationToShortcut;// The path of the file that will launch when the shortcut is run
+shortcut.Save();// Save the shortcut
+}
+
+
+    }
+}
---
+++ NHA_CreateShortcut/NHA_OpenShortcut/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NHA_OpenShortcut{
+    internal class Program{
+        static void Main(string[] args){
+            if(args.Length == 0){
+                Environment.Exit(0);
+            }else{
+                var CMD = args[0].Split(',');
+                string Directory = CMD[0];
+                string Name = CMD[1];
+                var CommandFile = Name + ".cmd";
+                if(File.Exists(CommandFile))
+                    File.Delete(CommandFile);
+
+                File.WriteAllText(CommandFile, 
+                    "cd /d \""+ Directory + "\"\n" +
+                    "\""+Name+".lnk\"" 
+                  //  +"\n" +"pause"
+                    );
+
+                Process P = new Process() { 
+                    StartInfo=new ProcessStartInfo(CommandFile) { 
+                    
+                    }
+                };
+                P.Exited += (X, E) => {
+                    
+                    try{
+                    if (File.Exists(CommandFile))
+                        File.Delete(CommandFile);
+                    }catch(Exception e){
+
+                    }
+                    Environment.Exit(0);
+                };
+                P.Start();
+
+                Thread.Sleep(400);
+                if(!P.HasExited)
+                P.Kill();
+                Thread.Sleep(400);
+                    try{
+                    if (File.Exists(CommandFile))
+                        File.Delete(CommandFile);
+                    }catch(Exception e){
+
+                }
+                Environment.Exit(0);
+
+            }
+        }
+    }
+}
